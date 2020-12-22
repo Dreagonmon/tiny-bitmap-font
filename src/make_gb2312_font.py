795bcd3a43ea0b86703ff2b8b38604c68a728b27
Dreagonmon <531486058@qq.com>
2020-12-22 16:21:08 +0800

    add font make script

--- src/make_gb2312_font.py
+++ src/make_gb2312_font.py
@@ -9,25 +9,65 @@ env.ensure_import_path()
 
 # import
 import coding, pbm, math
+try:
+    from PIL import Image
+    _support_pil = True
+except ImportError:
+    _support_pil = False
 
-def init_gb2312_src(size=8):
-    gb2312_folder = os.path.join(env.ROOT_FOLDER, "src", str(size), "gb2312")
-    data_size = math.ceil(size/8) * size
-    pbm_empty_data = bytearray(data_size)
-    for area, posi in coding.GB2312.all_available_pos():
-        area_folder = os.path.join(gb2312_folder, "{:02d}".format(area))
-        if not os.path.exists(area_folder):
-            os.makedirs(area_folder)
-        posi_file = os.path.join(area_folder, "{:02d}.pbm".format(posi))
-        char_str = coding.GB2312.to_bytes((area, posi)).decode("gb2312")
-        if not os.path.exists(posi_file):
-            with open(posi_file, "wb") as f:
-                pbm.make_image(f, size, size, pbm_empty_data, "P1", "gb2312 {} {} {}".format(area, posi, char_str))
-    # finished
+def interrupt(msg=None):
+    if msg:
+        print(msg)
+    sys.exit(1)
 
 def build_gb2312_hzk(size=8):
-    pass
+    gb2312_folder = os.path.join(env.ROOT_FOLDER, "src", str(size), "gb2312")
+    data_size = math.ceil(size/8) * size
+    area_range = 87
+    font_file_data = bytearray(data_size * area_range * 94)
+    if _support_pil:
+        preview = Image.new("1", (size*10, size*10*area_range), color=255)
+    else:
+        preview = None
+    for area, posi in coding.GB2312.all_available_pos():
+        area_folder = os.path.join(gb2312_folder, "{:02d}".format(area))
+        posi_file = os.path.join(area_folder, "{:02d}.pbm".format(posi))
+        if not os.path.exists(posi_file):
+            continue
+        try:
+            with open(posi_file, "rb") as in_stream:
+                w, h, format, pbm_data, _ = pbm.read_image(in_stream)
+            assert w == size and h == size
+            assert format == b"P1" or format == b"P4"
+            assert len(pbm_data) == data_size
+            data_offset = coding.GB2312.to_dict_index((area, posi)) * data_size
+            font_file_data[data_offset: data_offset+data_size] = pbm_data
+            if _support_pil:
+                #draw preview
+                for i in range(data_size):
+                    pbm_data[i] = (~pbm_data[i]) & 0xFF
+                fnt_img = Image.frombuffer("1", (w, h), bytes(pbm_data))
+                preview_pos = (((posi-1) % 10) * w, ((area-1) * 10 * h) + ((posi-1) // 10) * h)
+                preview.paste(fnt_img, preview_pos)
+        except:
+            import traceback
+            traceback.print_exc()
+            interrupt("Not a .pbm file: {}".format(posi_file))
+        char_str = coding.GB2312.to_bytes((area, posi)).decode("gb2312")
+        print("Processing: a:{} p:{} c:{}".format(area, posi, char_str), end="\r")
+    # optput
+    out_folder = os.path.join(env.ROOT_FOLDER, "build", "gb2312")
+    out_path = os.path.join(out_folder, "TinyBitmap-{}x{}.fnt".format(size, size))
+    if not os.path.exists(out_folder):
+        os.makedirs(out_folder)
+    with open(out_path, "wb") as f:
+        f.write(font_file_data)
+    print("Font file located at: {}".format(out_path))
+    if preview != None:
+        prev_path = os.path.join(out_folder, "TinyBitmap-{}x{}.bmp".format(size, size))
+        preview.save(prev_path)
+        print("Preview image located at: {}".format(prev_path))
 
 if __name__ == "__main__":
-    # init_gb2312_src(8)
+    build_gb2312_hzk(8)
     pass
